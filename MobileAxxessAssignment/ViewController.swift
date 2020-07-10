6cf646bb76612f7b18c6e8c9ae0b510e99b06852
Tirupati Balan <tirupati.balan@gmail.com>
2020-07-10 07:59:35 +0530

    Initial Commit

---
+++ MobileAxxessAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileAxxessAssignment
+//
+//  Created by Tirupati Balan on 10/07/20.
+//  Copyright © 2020 Celerstudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
